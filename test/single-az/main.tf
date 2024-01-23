dbd6fc1fde1cb7c50ef7822653d0763de9606441
Dustin Scott <dustin.scott18@gmail.com>
2024-01-23 13:52:48 -0600

    feat: add region config
    Signed-off-by: Dustin Scott <dustin.scott18@gmail.com>

--- test/single-az/main.tf
+++ test/single-az/main.tf
@@ -1,17 +1,19 @@
 provider "aws" {
-  region = "us-east-2"
+  region = "us-east-1"
 }
 
 module "test" {
   source = "../../"
 
+  cluster_name = "dscott-small"
+
   network = {
     private_link       = true
     multi_az           = false
     vpc_network        = "10.10.0.0"
-    vpc_cidr_size      = 16
+    vpc_cidr_size      = 23
     private_subnet_ids = []
     public_subnet_ids  = []
-    subnet_cidr_size   = 20
+    subnet_cidr_size   = 26
   }
 }
