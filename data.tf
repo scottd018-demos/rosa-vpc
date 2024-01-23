583b7734863de4c58e315ae34d4adeb44fee37b3
Dustin Scott <dustin.scott18@gmail.com>
2024-01-23 14:55:44 -0600

    fix: use region input
    Signed-off-by: Dustin Scott <dustin.scott18@gmail.com>

--- data.tf
+++ data.tf
@@ -1,14 +1,12 @@
-data "aws_region" "current" {}
-
 data "aws_availability_zones" "available" {
   filter {
     name   = "region-name"
-    values = [data.aws_region.current.name]
+    values = [var.region]
   }
 
   filter {
     name   = "group-name"
-    values = [data.aws_region.current.name]
+    values = [var.region]
   }
 
   filter {
